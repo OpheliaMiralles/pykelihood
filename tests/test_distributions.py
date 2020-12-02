b3b82d2dcd9dbba9461c07d7da9da74bbe06d3bc
OpheliaMiralles <opheliamiralles@gmail.com>
2020-12-02 13:51:05 +0100

    test

--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -3,7 +3,7 @@ import pytest
 from scipy.stats import genextreme
 
 from pykelihood import distributions, kernels
-from pykelihood.distributions import GEV
+from pykelihood.distributions import GEV, Normal
 
 REL_PREC = 1e-7
 ABS_PREC = 0.1
@@ -45,3 +45,12 @@ class TestGEV:
             assert fit.loc == approx(loc)
             assert fit.scale == approx(scale)
             assert fit.shape == approx(-c)
+
+    def test_fixed_values(self):
+        data = np.random.standard_normal(1000)
+        raw = Normal.fit(data)
+        assert raw.loc == approx(0.)
+        assert raw.scale == approx(1.)
+        fixed = Normal.fit(data, loc=1.)
+        assert fixed.loc == 1.
+
