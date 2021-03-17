63516cc0a14c70666c2b93798d1667eb0288f075
OpheliaMiralles <opheliamiralles@gmail.com>
2021-03-17 19:04:35 +0100

    qq plot with exponential and frechet scales

--- pykelihood/visualisation/utils.py
+++ pykelihood/visualisation/utils.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 matplotlib.rcParams["text.usetex"] = True
 
-from pykelihood.distributions import Distribution, Uniform
+from pykelihood.distributions import GEV, Distribution, Exponential, Uniform
 from pykelihood.stats_utils import Likelihood
 
 warnings.filterwarnings("ignore")
@@ -54,11 +54,11 @@ def get_quantiles_and_confidence_intervals(
     return theoretical, empirical, min_max["lower_bound"], min_max["upper_bound"]
 
 
-def qq_plot_uniform_scale(
+def pp_plot(
     fit: Distribution,
     data: Union[pd.DataFrame, np.array, pd.Series],
     path_to_figure: str = None,
-    figure_name="qq_plot",
+    figure_name="pp_plot",
     ci_confidence=0.99,
 ):
     (
@@ -75,6 +75,7 @@ def qq_plot_uniform_scale(
     plt.xlabel(f"Theoretical quantiles ({n} observations)")
     plt.ylabel("Empirical quantiles")
     plt.tight_layout()
+    plt.title("PP Plot")
     if path_to_figure is None:
         path_to_figure = __file__
     plt.savefig(f"{path_to_figure}/{figure_name}.png")
@@ -94,8 +95,6 @@ def qq_plot(
         lower_bound,
         upper_bound,
     ) = get_quantiles_and_confidence_intervals(fit, data, ci_confidence)
-    import matplotlib.pyplot as plt
-
     n = len(data)
     plt.scatter(theoretical, empirical, s=5, color="navy")
     plt.plot(theoretical, theoretical, label=f"$x=y$", color="navy")
@@ -106,6 +105,73 @@ def qq_plot(
     plt.xlabel(f"Theoretical quantiles ({n} observations)")
     plt.ylabel("Empirical quantiles")
     plt.tight_layout()
+    plt.title("QQ Plot")
+    if path_to_figure is None:
+        path_to_figure = __file__
+    plt.savefig(f"{path_to_figure}/{figure_name}.png")
+    plt.clf()
+
+
+def qq_plot_exponential_scale(
+    fit: Distribution,
+    data: Union[pd.DataFrame, np.array, pd.Series],
+    path_to_figure: str = None,
+    figure_name="qq_plot",
+    ci_confidence=0.99,
+):
+    (
+        theoretical,
+        empirical,
+        lower_bound,
+        upper_bound,
+    ) = get_quantiles_and_confidence_intervals_uniform_scale(fit, data, ci_confidence)
+    unit_exp = Exponential()
+    theo_exp = unit_exp.inverse_cdf(theoretical)
+    empi_exp = unit_exp.inverse_cdf(empirical)
+    lb_exp = unit_exp.inverse_cdf(lower_bound)
+    ub_exp = unit_exp.inverse_cdf(upper_bound)
+    n = len(data)
+    plt.scatter(theo_exp, empi_exp, s=5, color="navy")
+    plt.plot(theo_exp, theo_exp, label=f"$x=y$", color="navy")
+    plt.fill_betweenx(y=empi_exp, x1=lb_exp, x2=ub_exp, alpha=0.2, color="navy")
+    plt.legend()
+    plt.xlabel(f"Theoretical unit Exponential quantiles ({n} observations)")
+    plt.ylabel("Empirical quantiles")
+    plt.tight_layout()
+    plt.title("QQ Plot: Unit Exponential Scale")
+    if path_to_figure is None:
+        path_to_figure = __file__
+    plt.savefig(f"{path_to_figure}/{figure_name}.png")
+    plt.clf()
+
+
+def qq_plot_frechet_scale(
+    fit: Distribution,
+    data: Union[pd.DataFrame, np.array, pd.Series],
+    path_to_figure: str = None,
+    figure_name="qq_plot",
+    ci_confidence=0.99,
+):
+    (
+        theoretical,
+        empirical,
+        lower_bound,
+        upper_bound,
+    ) = get_quantiles_and_confidence_intervals_uniform_scale(fit, data, ci_confidence)
+    unit_frechet = GEV(1, 1, -1)
+    theo_fr = unit_frechet.inverse_cdf(theoretical)
+    empi_fr = unit_frechet.inverse_cdf(empirical)
+    lb_fr = unit_frechet.inverse_cdf(lower_bound)
+    ub_fr = unit_frechet.inverse_cdf(upper_bound)
+    n = len(data)
+    plt.scatter(theo_fr, empi_fr, s=5, color="navy")
+    plt.plot(theo_fr, theo_fr, label=f"$x=y$", color="navy")
+    plt.fill_betweenx(y=empi_fr, x1=lb_fr, x2=ub_fr, alpha=0.2, color="navy")
+    plt.legend()
+    plt.xlabel(r"Theoretical unit Fr\'echet quantiles ({} observations)".format(n))
+    plt.ylabel("Empirical quantiles")
+    plt.tight_layout()
+    plt.title(r"QQ Plot: Unit Fr\'echet Scale")
     if path_to_figure is None:
         path_to_figure = __file__
     plt.savefig(f"{path_to_figure}/{figure_name}.png")
