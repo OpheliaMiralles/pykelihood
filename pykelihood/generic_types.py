17fa5b1c0dbc807724d565cfbd0d38c9b2f8f180
jonathanberthias <jvberthias@gmail.com>
2021-04-08 11:19:56 +0200

    Improve Obs type

--- pykelihood/generic_types.py
+++ pykelihood/generic_types.py
@@ -1,6 +1,3 @@
-from typing import Union
+from typing import Sequence, Union
 
-import numpy as np
-import pandas as pd
-
-Obs = Union[float, np.ndarray, pd.Series]
+Obs = Union[float, Sequence[float]]
