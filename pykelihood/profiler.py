571e3f0b4b60f4d2efa3654f68425589d6da6c9f
OpheliaMiralles <opheliamiralles@gmail.com>
2022-10-14 17:14:52 +0200

    refactored stats_utils

---
+++ pykelihood/profiler.py
@@ -0,0 +1,141 @@
+from __future__ import annotations
+
+import math
+import warnings
+from itertools import count
+from typing import Callable
+
+import numpy as np
+import pandas as pd
+from scipy.stats import chi2, f
+
+from pykelihood.cached_property import cached_property
+from pykelihood.distributions import Distribution, Normal
+from pykelihood.metrics import (
+    opposite_log_likelihood,
+)
+
+warnings.filterwarnings("ignore")
+
+
+class Profiler(object):
+    def __init__(
+            self,
+            distribution: Distribution,
+            data: pd.Series,
+            score_function: Callable = opposite_log_likelihood,
+            name: str = "Standard",
+            inference_confidence: float = 0.99,
+            single_profiling_param=None,
+            biv=False,
+    ):
+        """l
+
+        :param distribution: distribution on which the inference is based
+        :param data: variable of interest
+        :param score_function: function used for optimisation
+        :param name: name (optional) of the likelihood if it needs to be compared to other likelihood functions
+        :param inference_confidence: wanted confidence for intervals
+        :param fit_chi2: whether the results from the likelihood ratio method must be fitted to a chi2
+        or a generic chi2 with degree of freedom 1 is used
+        :param single_profiling_param: parameter that we want to fix to create the profiles based on likelihood
+        """
+        self.name = name
+        self.distribution = distribution
+        self.data = data
+        self.score_function = score_function
+        self.inference_confidence = inference_confidence
+        self.single_profiling_param = single_profiling_param
+        self.biv = biv
+
+    @cached_property
+    def standard_mle(self):
+        estimate = self.distribution.fit(self.data)
+        ll = -opposite_log_likelihood(estimate, self.data)
+        ll = ll if isinstance(ll, float) else ll[0]
+        return (estimate, ll)
+
+    @cached_property
+    def optimum(self):
+        x0 = self.distribution.optimisation_params
+        estimate = self.distribution.fit_instance(
+            self.data, score=self.score_function, x0=x0
+        )
+        func = -self.score_function(estimate, self.data)
+        func = func if isinstance(func, float) else func[0]
+        return (estimate, func)
+
+    @cached_property
+    def profiles(self):
+        profiles = {}
+        opt, func = self.optimum
+        if self.single_profiling_param is not None:
+            params = [self.single_profiling_param]
+        else:
+            params = [n[0] for (v, n) in opt.param_mapping()]
+        for name, k in opt.optimisation_param_dict.items():
+            if name in params:
+                r = float(k)
+                sigma = (
+                    2 * (10 ** math.floor(math.log10(np.abs(r))))
+                    if name != "shape"
+                    else 0.25
+                )
+                range = Normal(r, sigma).ppf(np.linspace(1e-4, 1 - 1e-4, 20))
+                profiles[name] = self.test_profile_likelihood(range, name)
+        return profiles
+
+    def test_profile_likelihood(self, range_for_param, param):
+        opt, func = self.optimum
+        profile_ll = []
+        params = []
+        for x in range_for_param:
+            try:
+                pl = opt.fit_instance(
+                    self.data,
+                    score=self.score_function,
+                    **{param: x},
+                )
+                pl_value = -self.score_function(pl, self.data)
+                pl_value = pl_value if isinstance(pl_value, float) else pl_value[0]
+                if np.isfinite(pl_value):
+                    profile_ll.append(pl_value)
+                    params.append([p.value for p in pl.flattened_params])
+            except:
+                pass
+        if not self.biv:
+            chi2_par = {"df": 1}
+            lower_bound = func - chi2.ppf(self.inference_confidence, **chi2_par) / 2
+        else:
+            lower_bound = func - f(2, 2).ppf(self.inference_confidence)
+        filtered_params = pd.DataFrame(
+            [x + [eval] for x, eval in zip(params, profile_ll) if eval >= lower_bound]
+        )
+        cols = list(opt.flattened_param_dict.keys()) + ["score"]
+        filtered_params = filtered_params.rename(columns=dict(zip(count(), cols)))
+        return filtered_params
+
+    def confidence_interval(self, metric: Callable[[Distribution], float]):
+        """
+
+        :param metric: function depending on the distribution: it can be one of the parameter (ex: lambda x: x.shape() for a parameter called "shape"),
+        or a metric relevant to the field of study (ex: the 100-years return level for extreme value analysis by setting lambda x: x.isf(1/100))...
+        :return: bounds based on parameter profiles for this metric
+        """
+        estimates = []
+        profiles = self.profiles
+        params = list(profiles.keys())
+        for param in params:
+            columns = [
+                n[0] for (v, n) in self.optimum[0].param_mapping() if n[0] in self.optimum[0].optimisation_param_dict]
+            result = profiles[param].apply(
+                lambda row: metric(
+                    self.distribution.with_params({k: row[k] for k in columns}.values())
+                ),
+                axis=1,
+            )
+            estimates.extend(list(result.values))
+        if len(estimates):
+            return [np.min(estimates), np.max(estimates)]
+        else:
+            return [-np.inf, np.inf]
