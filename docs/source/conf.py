5281bfe3bb5dde0bc05b387722f7c6f08950cc1e
OpheliaMiralles <opheliamiralles@gmail.com>
2025-01-28 20:49:00 +0100

    Remove sys path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,10 +5,6 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../src'))
-
 project = "pykelihood"
 copyright = "2025, Ophélia Miralles"
 author = "Ophélia Miralles"
