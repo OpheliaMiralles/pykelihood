039330f85f6474ecfce5d36c0d73bc14c2d44f7f
Jonathan Berthias <jvberthias@gmail.com>
2025-06-14 15:22:57 +0200

    Cleanup dynamic distribution class creation

--- pykelihood/distributions/__init__.py
+++ pykelihood/distributions/__init__.py
@@ -1,7 +1,6 @@
-from pykelihood.distributions.scipy import *
-from pykelihood.distributions.custom import *
+from pykelihood.distributions import custom, scipy
 from pykelihood.distributions.base import *
-from pykelihood.distributions import scipy
-from pykelihood.distributions import custom
+from pykelihood.distributions.custom import *
+from pykelihood.distributions.scipy import *
 
 __all__ = [*scipy.__all__, *custom.__all__, "Distribution", "ScipyDistribution"]
