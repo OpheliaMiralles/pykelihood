505bc95dde28388280ea0a9148b4790b0cfc7eb2
Jonathan Berthias <jvberthias@gmail.com>
2025-06-15 21:26:41 +0200

    Cleanup dynamic distribution class creation (#47)
    * Cleanup dynamic distribution class creation
    
    * Rename Inner to Wrapper

--- pykelihood/distributions/scipy.py
+++ pykelihood/distributions/scipy.py
@@ -13,13 +13,13 @@ def _name_from_scipy_dist(scipy_dist: stats.rv_continuous) -> str:
     return "".join(map(str.capitalize, scipy_dist_name.split("_")))
 
 
-def _wrap_scipy_distribution(
+def wrap_scipy_distribution(
     scipy_dist: stats.rv_continuous,
 ) -> type[ScipyDistribution]:
     """Wrap a scipy distribution class to create a ScipyDistribution subclass."""
     scipy_dist_name = type(scipy_dist).__name__.removesuffix("_gen")
     clean_dist_name = _name_from_scipy_dist(scipy_dist)
-    params_names = ("loc", "scale") + tuple(
+    dist_params_names = ("loc", "scale") + tuple(
         scipy_dist.shapes.split(", ") if scipy_dist.shapes else ()
     )
 
@@ -40,151 +40,151 @@ def _wrap_scipy_distribution(
         Shape parameter. See the SciPy documentation for the {scipy_dist_name} distribution for details.\
         """
 
-    for param in params_names[2:]:
+    for param in dist_params_names[2:]:
         docstring += format_param_docstring(param)
 
-    def __init__(self, loc=0.0, scale=1.0, **kwargs):
-        shape_args = params_names[2:]
-        for arg in shape_args:
-            if arg not in kwargs:
-                raise ValueError(f"Missing shape parameter: {arg}")
-        args = [kwargs[a] for a in shape_args]
-        ScipyDistribution.__init__(self, loc, scale, *args)
+    class Wrapper(ScipyDistribution):
+        _base_module = scipy_dist
+        params_names = dist_params_names
+        __doc__ = docstring
 
-    def _to_scipy_args(self, **kwargs):
-        return {k: kwargs.get(k, getattr(self, k)()) for k in self.params_names}
+        def __init__(self, loc=0.0, scale=1.0, **kwargs):
+            assert self.params_names[:2] == ("loc", "scale")
+            shape_args = self.params_names[2:]
+            for arg in shape_args:
+                if arg not in kwargs:
+                    raise ValueError(
+                        f"Missing shape parameter `{arg}` when initializing {type(self).__name__} distribution."
+                    )
+            args = [kwargs[a] for a in shape_args]
+            super().__init__(loc, scale, *args)
 
-    return type(
-        clean_dist_name,
-        (ScipyDistribution,),
-        {
-            "_base_module": scipy_dist,
-            "params_names": params_names,
-            "__init__": __init__,
-            "_to_scipy_args": _to_scipy_args,
-            "__doc__": docstring,
-        },
-    )
+        def _to_scipy_args(self, **kwargs):
+            return {k: kwargs.get(k, getattr(self, k)()) for k in self.params_names}
+
+    Wrapper.__name__ = clean_dist_name
+    Wrapper.__qualname__ = f"{Wrapper.__module__}.{Wrapper.__name__}"
+    return Wrapper
 
 
-Alpha = _wrap_scipy_distribution(stats.alpha)
-Anglit = _wrap_scipy_distribution(stats.anglit)
-Arcsine = _wrap_scipy_distribution(stats.arcsine)
-Argus = _wrap_scipy_distribution(stats.argus)
-# Beta = _wrap_scipy_distribution(stats.beta)
-Betaprime = _wrap_scipy_distribution(stats.betaprime)
-Bradford = _wrap_scipy_distribution(stats.bradford)
-Burr = _wrap_scipy_distribution(stats.burr)
-Burr12 = _wrap_scipy_distribution(stats.burr12)
-Cauchy = _wrap_scipy_distribution(stats.cauchy)
-Chi = _wrap_scipy_distribution(stats.chi)
-Chi2 = _wrap_scipy_distribution(stats.chi2)
-Cosine = _wrap_scipy_distribution(stats.cosine)
-Crystalball = _wrap_scipy_distribution(stats.crystalball)
-Dgamma = _wrap_scipy_distribution(stats.dgamma)
-Dweibull = _wrap_scipy_distribution(stats.dweibull)
-Erlang = _wrap_scipy_distribution(stats.erlang)
-Expon = _wrap_scipy_distribution(stats.expon)
-Exponnorm = _wrap_scipy_distribution(stats.exponnorm)
-Exponpow = _wrap_scipy_distribution(stats.exponpow)
-Exponweib = _wrap_scipy_distribution(stats.exponweib)
-F = _wrap_scipy_distribution(stats.f)
-Fatiguelife = _wrap_scipy_distribution(stats.fatiguelife)
-Fisk = _wrap_scipy_distribution(stats.fisk)
-Foldcauchy = _wrap_scipy_distribution(stats.foldcauchy)
-Foldnorm = _wrap_scipy_distribution(stats.foldnorm)
-# Gamma = _wrap_scipy_distribution(stats.gamma)
-Gausshyper = _wrap_scipy_distribution(stats.gausshyper)
-Genexpon = _wrap_scipy_distribution(stats.genexpon)
-Genextreme = _wrap_scipy_distribution(stats.genextreme)
-Gengamma = _wrap_scipy_distribution(stats.gengamma)
-Genhalflogistic = _wrap_scipy_distribution(stats.genhalflogistic)
-Genhyperbolic = _wrap_scipy_distribution(stats.genhyperbolic)
-Geninvgauss = _wrap_scipy_distribution(stats.geninvgauss)
-Genlogistic = _wrap_scipy_distribution(stats.genlogistic)
-Gennorm = _wrap_scipy_distribution(stats.gennorm)
-Genpareto = _wrap_scipy_distribution(stats.genpareto)
-Gibrat = _wrap_scipy_distribution(stats.gibrat)
-Gompertz = _wrap_scipy_distribution(stats.gompertz)
-GumbelL = _wrap_scipy_distribution(stats.gumbel_l)
-GumbelR = _wrap_scipy_distribution(stats.gumbel_r)
-Halfcauchy = _wrap_scipy_distribution(stats.halfcauchy)
-Halfgennorm = _wrap_scipy_distribution(stats.halfgennorm)
-Halflogistic = _wrap_scipy_distribution(stats.halflogistic)
-Halfnorm = _wrap_scipy_distribution(stats.halfnorm)
-Hypsecant = _wrap_scipy_distribution(stats.hypsecant)
-Invgamma = _wrap_scipy_distribution(stats.invgamma)
-Invgauss = _wrap_scipy_distribution(stats.invgauss)
-Invweibull = _wrap_scipy_distribution(stats.invweibull)
-JfSkewT = _wrap_scipy_distribution(stats.jf_skew_t)
-Johnsonsb = _wrap_scipy_distribution(stats.johnsonsb)
-Johnsonsu = _wrap_scipy_distribution(stats.johnsonsu)
-Kappa3 = _wrap_scipy_distribution(stats.kappa3)
-Kappa4 = _wrap_scipy_distribution(stats.kappa4)
-Ksone = _wrap_scipy_distribution(stats.ksone)
-Kstwo = _wrap_scipy_distribution(stats.kstwo)
-Kstwobign = _wrap_scipy_distribution(stats.kstwobign)
-Laplace = _wrap_scipy_distribution(stats.laplace)
-LaplaceAsymmetric = _wrap_scipy_distribution(stats.laplace_asymmetric)
-Levy = _wrap_scipy_distribution(stats.levy)
-LevyL = _wrap_scipy_distribution(stats.levy_l)
-LevyStable = _wrap_scipy_distribution(stats.levy_stable)
-Loggamma = _wrap_scipy_distribution(stats.loggamma)
-Logistic = _wrap_scipy_distribution(stats.logistic)
-Loglaplace = _wrap_scipy_distribution(stats.loglaplace)
-Lognorm = _wrap_scipy_distribution(stats.lognorm)
-Lomax = _wrap_scipy_distribution(stats.lomax)
-Maxwell = _wrap_scipy_distribution(stats.maxwell)
-Mielke = _wrap_scipy_distribution(stats.mielke)
-Moyal = _wrap_scipy_distribution(stats.moyal)
-Nakagami = _wrap_scipy_distribution(stats.nakagami)
-Ncf = _wrap_scipy_distribution(stats.ncf)
-Nct = _wrap_scipy_distribution(stats.nct)
-Ncx2 = _wrap_scipy_distribution(stats.ncx2)
-Norm = _wrap_scipy_distribution(stats.norm)
+Alpha = wrap_scipy_distribution(stats.alpha)
+Anglit = wrap_scipy_distribution(stats.anglit)
+Arcsine = wrap_scipy_distribution(stats.arcsine)
+Argus = wrap_scipy_distribution(stats.argus)
+# Beta = wrap_scipy_distribution(stats.beta)
+Betaprime = wrap_scipy_distribution(stats.betaprime)
+Bradford = wrap_scipy_distribution(stats.bradford)
+Burr = wrap_scipy_distribution(stats.burr)
+Burr12 = wrap_scipy_distribution(stats.burr12)
+Cauchy = wrap_scipy_distribution(stats.cauchy)
+Chi = wrap_scipy_distribution(stats.chi)
+Chi2 = wrap_scipy_distribution(stats.chi2)
+Cosine = wrap_scipy_distribution(stats.cosine)
+Crystalball = wrap_scipy_distribution(stats.crystalball)
+Dgamma = wrap_scipy_distribution(stats.dgamma)
+Dweibull = wrap_scipy_distribution(stats.dweibull)
+Erlang = wrap_scipy_distribution(stats.erlang)
+Expon = wrap_scipy_distribution(stats.expon)
+Exponnorm = wrap_scipy_distribution(stats.exponnorm)
+Exponpow = wrap_scipy_distribution(stats.exponpow)
+Exponweib = wrap_scipy_distribution(stats.exponweib)
+F = wrap_scipy_distribution(stats.f)
+Fatiguelife = wrap_scipy_distribution(stats.fatiguelife)
+Fisk = wrap_scipy_distribution(stats.fisk)
+Foldcauchy = wrap_scipy_distribution(stats.foldcauchy)
+Foldnorm = wrap_scipy_distribution(stats.foldnorm)
+# Gamma = wrap_scipy_distribution(stats.gamma)
+Gausshyper = wrap_scipy_distribution(stats.gausshyper)
+Genexpon = wrap_scipy_distribution(stats.genexpon)
+Genextreme = wrap_scipy_distribution(stats.genextreme)
+Gengamma = wrap_scipy_distribution(stats.gengamma)
+Genhalflogistic = wrap_scipy_distribution(stats.genhalflogistic)
+Genhyperbolic = wrap_scipy_distribution(stats.genhyperbolic)
+Geninvgauss = wrap_scipy_distribution(stats.geninvgauss)
+Genlogistic = wrap_scipy_distribution(stats.genlogistic)
+Gennorm = wrap_scipy_distribution(stats.gennorm)
+Genpareto = wrap_scipy_distribution(stats.genpareto)
+Gibrat = wrap_scipy_distribution(stats.gibrat)
+Gompertz = wrap_scipy_distribution(stats.gompertz)
+GumbelL = wrap_scipy_distribution(stats.gumbel_l)
+GumbelR = wrap_scipy_distribution(stats.gumbel_r)
+Halfcauchy = wrap_scipy_distribution(stats.halfcauchy)
+Halfgennorm = wrap_scipy_distribution(stats.halfgennorm)
+Halflogistic = wrap_scipy_distribution(stats.halflogistic)
+Halfnorm = wrap_scipy_distribution(stats.halfnorm)
+Hypsecant = wrap_scipy_distribution(stats.hypsecant)
+Invgamma = wrap_scipy_distribution(stats.invgamma)
+Invgauss = wrap_scipy_distribution(stats.invgauss)
+Invweibull = wrap_scipy_distribution(stats.invweibull)
+JfSkewT = wrap_scipy_distribution(stats.jf_skew_t)
+Johnsonsb = wrap_scipy_distribution(stats.johnsonsb)
+Johnsonsu = wrap_scipy_distribution(stats.johnsonsu)
+Kappa3 = wrap_scipy_distribution(stats.kappa3)
+Kappa4 = wrap_scipy_distribution(stats.kappa4)
+Ksone = wrap_scipy_distribution(stats.ksone)
+Kstwo = wrap_scipy_distribution(stats.kstwo)
+Kstwobign = wrap_scipy_distribution(stats.kstwobign)
+Laplace = wrap_scipy_distribution(stats.laplace)
+LaplaceAsymmetric = wrap_scipy_distribution(stats.laplace_asymmetric)
+Levy = wrap_scipy_distribution(stats.levy)
+LevyL = wrap_scipy_distribution(stats.levy_l)
+LevyStable = wrap_scipy_distribution(stats.levy_stable)
+Loggamma = wrap_scipy_distribution(stats.loggamma)
+Logistic = wrap_scipy_distribution(stats.logistic)
+Loglaplace = wrap_scipy_distribution(stats.loglaplace)
+Lognorm = wrap_scipy_distribution(stats.lognorm)
+Lomax = wrap_scipy_distribution(stats.lomax)
+Maxwell = wrap_scipy_distribution(stats.maxwell)
+Mielke = wrap_scipy_distribution(stats.mielke)
+Moyal = wrap_scipy_distribution(stats.moyal)
+Nakagami = wrap_scipy_distribution(stats.nakagami)
+Ncf = wrap_scipy_distribution(stats.ncf)
+Nct = wrap_scipy_distribution(stats.nct)
+Ncx2 = wrap_scipy_distribution(stats.ncx2)
+Norm = wrap_scipy_distribution(stats.norm)
 Normal = Norm  # alias for backward compatibility
-Norminvgauss = _wrap_scipy_distribution(stats.norminvgauss)
-# Pareto = _wrap_scipy_distribution(stats.pareto)
-Pearson3 = _wrap_scipy_distribution(stats.pearson3)
-Powerlaw = _wrap_scipy_distribution(stats.powerlaw)
-Powerlognorm = _wrap_scipy_distribution(stats.powerlognorm)
-Powernorm = _wrap_scipy_distribution(stats.powernorm)
-Rayleigh = _wrap_scipy_distribution(stats.rayleigh)
-Rdist = _wrap_scipy_distribution(stats.rdist)
-Recipinvgauss = _wrap_scipy_distribution(stats.recipinvgauss)
-Loguniform = _wrap_scipy_distribution(stats.loguniform)
-Reciprocal = _wrap_scipy_distribution(stats.reciprocal)
-RelBreitwigner = _wrap_scipy_distribution(stats.rel_breitwigner)
-Rice = _wrap_scipy_distribution(stats.rice)
-Semicircular = _wrap_scipy_distribution(stats.semicircular)
-Skewcauchy = _wrap_scipy_distribution(stats.skewcauchy)
-Skewnorm = _wrap_scipy_distribution(stats.skewnorm)
-StudentizedRange = _wrap_scipy_distribution(stats.studentized_range)
-T = _wrap_scipy_distribution(stats.t)
-Trapezoid = _wrap_scipy_distribution(stats.trapezoid)
+Norminvgauss = wrap_scipy_distribution(stats.norminvgauss)
+# Pareto = wrap_scipy_distribution(stats.pareto)
+Pearson3 = wrap_scipy_distribution(stats.pearson3)
+Powerlaw = wrap_scipy_distribution(stats.powerlaw)
+Powerlognorm = wrap_scipy_distribution(stats.powerlognorm)
+Powernorm = wrap_scipy_distribution(stats.powernorm)
+Rayleigh = wrap_scipy_distribution(stats.rayleigh)
+Rdist = wrap_scipy_distribution(stats.rdist)
+Recipinvgauss = wrap_scipy_distribution(stats.recipinvgauss)
+Loguniform = wrap_scipy_distribution(stats.loguniform)
+Reciprocal = wrap_scipy_distribution(stats.reciprocal)
+RelBreitwigner = wrap_scipy_distribution(stats.rel_breitwigner)
+Rice = wrap_scipy_distribution(stats.rice)
+Semicircular = wrap_scipy_distribution(stats.semicircular)
+Skewcauchy = wrap_scipy_distribution(stats.skewcauchy)
+Skewnorm = wrap_scipy_distribution(stats.skewnorm)
+StudentizedRange = wrap_scipy_distribution(stats.studentized_range)
+T = wrap_scipy_distribution(stats.t)
+Trapezoid = wrap_scipy_distribution(stats.trapezoid)
 Trapz = Trapezoid
-Triang = _wrap_scipy_distribution(stats.triang)
-Truncexpon = _wrap_scipy_distribution(stats.truncexpon)
-Truncnorm = _wrap_scipy_distribution(stats.truncnorm)
-Truncpareto = _wrap_scipy_distribution(stats.truncpareto)
-TruncweibullMin = _wrap_scipy_distribution(stats.truncweibull_min)
-Tukeylambda = _wrap_scipy_distribution(stats.tukeylambda)
-Uniform = _wrap_scipy_distribution(stats.uniform)
-Vonmises = _wrap_scipy_distribution(stats.vonmises)
-VonmisesLine = _wrap_scipy_distribution(stats.vonmises_line)
-Wald = _wrap_scipy_distribution(stats.wald)
-WeibullMax = _wrap_scipy_distribution(stats.weibull_max)
-WeibullMin = _wrap_scipy_distribution(stats.weibull_min)
-Wrapcauchy = _wrap_scipy_distribution(stats.wrapcauchy)
+Triang = wrap_scipy_distribution(stats.triang)
+Truncexpon = wrap_scipy_distribution(stats.truncexpon)
+Truncnorm = wrap_scipy_distribution(stats.truncnorm)
+Truncpareto = wrap_scipy_distribution(stats.truncpareto)
+TruncweibullMin = wrap_scipy_distribution(stats.truncweibull_min)
+Tukeylambda = wrap_scipy_distribution(stats.tukeylambda)
+Uniform = wrap_scipy_distribution(stats.uniform)
+Vonmises = wrap_scipy_distribution(stats.vonmises)
+VonmisesLine = wrap_scipy_distribution(stats.vonmises_line)
+Wald = wrap_scipy_distribution(stats.wald)
+WeibullMax = wrap_scipy_distribution(stats.weibull_max)
+WeibullMin = wrap_scipy_distribution(stats.weibull_min)
+Wrapcauchy = wrap_scipy_distribution(stats.wrapcauchy)
 
 if Version(scipy.__version__) >= Version("1.15.0"):
-    DparetoLognorm = _wrap_scipy_distribution(stats.dpareto_lognorm)
-    Landau = _wrap_scipy_distribution(stats.landau)
-    Irwinhall = _wrap_scipy_distribution(stats.irwinhall)
+    DparetoLognorm = wrap_scipy_distribution(stats.dpareto_lognorm)
+    Landau = wrap_scipy_distribution(stats.landau)
+    Irwinhall = wrap_scipy_distribution(stats.irwinhall)
 
 __all__ = [
     "_name_from_scipy_dist",
-    "_wrap_scipy_distribution",
+    "wrap_scipy_distribution",
     "Alpha",
     "Anglit",
     "Arcsine",
